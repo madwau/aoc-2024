7fa4aeec76d3de453d92325dc17f45e5c89928da
Martin Wauligmann <martin@wauligmann.com>
2024-12-09 21:44:30 +0700

    Day 9 part 1

---
+++ src/days/day-09/day-09-part-1.ts
@@ -0,0 +1,32 @@
+import { repeat } from '../../utils/array';
+import { readLine } from '../../utils/file';
+import { sum } from '../../utils/math';
+import { isNumber } from '../../utils/typescript';
+
+type DiskMap = number[];
+type FileSystem = (number | '.')[];
+
+const diskMap: DiskMap = (await readLine('day-09', 'input')).split('').map(Number);
+
+function getFileSystem(): FileSystem {
+  return diskMap.flatMap((digit, i) => repeat([i % 2 === 0 ? i / 2 : '.'], digit));
+}
+
+function compact(fileSystem: FileSystem): FileSystem {
+  return fileSystem.reduce((currentFileSystem, block, index) => {
+    if (block === '.') {
+      const lastDigitIndex = currentFileSystem.findLastIndex(isNumber);
+      if (lastDigitIndex > index) {
+        currentFileSystem[index] = currentFileSystem[lastDigitIndex];
+        currentFileSystem[lastDigitIndex] = '.';
+      }
+    }
+    return currentFileSystem;
+  }, fileSystem);
+}
+
+function checksum(fileSystem: FileSystem): number {
+  return sum(fileSystem.filter(isNumber).map((block, index) => index * block));
+}
+
+console.log(checksum(compact(getFileSystem())));
--- src/utils/typescript.ts
+++ src/utils/typescript.ts
@@ -1,3 +1,7 @@
 export function exhaustiveCheck(message: string, x: never): never {
   throw new Error(`${message}: ${x}`);
 }
+
+export function isNumber(x: any) {
+  return typeof x === 'number';
+}
